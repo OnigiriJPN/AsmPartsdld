03676456ee1a929728dd420854ae2351434e0d9b
OnigiriJPN <yuzuhajintian@gmail.com>
2026-05-30 14:02:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-c
+const aTag = document.createElement("a");
+  aTag.href = null
+  aTag.download = "AsmPartsSetup"
+
+function clicked(){
+  try{
+    document.body.appendChild(aTag);
+    aTag.click();
+  }catch(e){
+    alert(`ダウンロードに失敗しました。
+    メッセージは以下の通りです。
+    ${e.message}`)
+  }
+}
